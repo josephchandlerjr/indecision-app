423b6f8ca7a48417d67d34f36be9760d10d850d0
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-02-03 11:49:56 -0500

    add babel and get started with a basic file structure

---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log('app.js is running')
+
+//JSX - JavaScript XML
+
+let appRoot = document.getElementById('app')
+let template = <h1>Indecision App</h1>
+ReactDOM.render(template, appRoot)
+
